ed0ce40bac31adecec673eaa0ad13843320e6b17
Jake Tae <jaesungtae@gmail.com>
2020-12-11 01:07:25 +0900

    feature: add dataset loaders & updated csv file

---
+++ dataset.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset, Subset
+
+
+class BlogDataset(Dataset):
+    def __init__(self, file_path):
+        self.data = pd.read_csv(file_path)
+
+    def __getitem__(self, index):
+        row = self.data.iloc[index]
+        return row["body"], torch.tensor(row[2:])
+
+    def __len__(self):
+        return len(self.data)
+
+
+def make_loaders(file_path, test_size, batch_size):
+    dataset = BlogDataset(file_path)
+    train_idx, test_idx = train_test_split(
+        range(len(dataset)), test_size=test_size, shuffle=True
+    )
+    train_dataset = Subset(dataset, indices=train_idx)
+    test_dataset = Subset(dataset, indices=test_idx)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
+
--- model.py
+++ model.py
@@ -1,5 +1,17 @@
 import torch
+from torch import nn
 import transformers as hf
 
 
-class DistilBert
+class DistilBertPostTagger(nn.Module):
+    def __init__(self, hidden_dim, num_tags, dropout=0.5):
+        super(DistilBertPostTagger, self).__init__()
+        self.bert = hf.DistilBertModel.from_pretrained("distilbert-base-uncased")
+        self.preclassifier = nn.Linear(hidden_dim, hidden_dim)
+        self.classifier = nn.Linear(hidden_dim, num_tags)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x):
+        bert_out = self.bert(x)
+        hidden_state = distilbert_output[0]
+        pooled_output = hidden_state[:, 0]
