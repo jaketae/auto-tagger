189ba28f54639164ac8de46d6b3c47aface61a4d
Jake Tae <jaesungtae@gmail.com>
2020-12-15 00:28:29 +0900

    feature: use clf token instead of pooler output

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class BertForPostClassification(nn.Module):
     def __init__(self, model_name, num_labels, dropout, freeze_bert=False):
         super(BertForPostClassification, self).__init__()
         self.bert = AutoModel.from_pretrained(model_name)
-        self.bert.config.return_dict = True
+        self.bert.config.return_dict = False
         self.bert.config.add_cross_attention = False
         self.bert.config.output_attentions = False
         self.bert.config.output_hidden_states = False
@@ -24,10 +24,7 @@ class BertForPostClassification(nn.Module):
 
     def forward(self, **tokens):
         bert_out = self.bert(**tokens)
-        try:
-            pooler_output = bert_out["pooler_output"]
-        except KeyError:
-            pooler_output = bert_out["last_hidden_state"][:, 0]
-        logits = self.classier(pooler_output)
+        clf_tokens = bert_out[0][:, 0, :]
+        logits = self.classier(clf_tokens)
         return logits
 
