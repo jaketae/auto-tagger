1f65ec569fbd62d209eec5b1a6cd70667a92da7a
Jake Tae <jaesungtae@gmail.com>
2020-12-15 08:27:21 +0900

    feature: bake tokenizer into the model

--- model.py
+++ model.py
@@ -1,16 +1,19 @@
 import torch.nn.functional as F
 from torch import nn
-from transformers import AutoModel
+from transformers import AutoModel, AutoTokenizer
 
 
 class BertForPostClassification(nn.Module):
     def __init__(self, model_name, num_labels, dropout, freeze_bert=False):
         super(BertForPostClassification, self).__init__()
-        self.bert = AutoModel.from_pretrained(model_name)
-        self.bert.config.return_dict = False
-        self.bert.config.add_cross_attention = False
-        self.bert.config.output_attentions = False
-        self.bert.config.output_hidden_states = False
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.bert = AutoModel.from_pretrained(
+            model_name,
+            return_dict=False,
+            add_cros_attention=False,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
         if freeze_bert:
             for param in self.bert.parameters():
                 param.requires_grad = False
@@ -22,7 +25,8 @@ class BertForPostClassification(nn.Module):
             nn.Linear(hidden_size, num_labels),
         )
 
-    def forward(self, **tokens):
+    def forward(self, x):
+        tokens = self.tokenizer(x)
         bert_out = self.bert(**tokens)
         clf_tokens = bert_out[0][:, 0, :]
         logits = self.classifier(clf_tokens)
