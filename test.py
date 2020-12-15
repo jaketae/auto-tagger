fix: correct hamming acc calculation formula

test.py
@@ -41,11 +41,11 @@ def get_hamming_accuracy(model, test_loader):
         for i in range(labels.size(0)):
             set_true = set(torch.where(labels[i])[0].tolist())
             set_pred = set(torch.where(outputs[i])[0].tolist())
-            denominator = len(set_true.intersection(set_pred))
+            denominator = len(set_true.union(set_pred))
             if denominator == 0:
                 scores.append(1)
             else:
-                numerator = len(set_true.union(set_pred))
+                numerator = len(set_true.intersection(set_pred))
                 scores.append(numerator / denominator)
     return sum(scores) / len(scores)
 