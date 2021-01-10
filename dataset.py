0c4ac7f559833f312c5cd40c1b311de4844e807e
Jake Tae <jaesungtae@gmail.com>
2021-01-10 18:00:47 +0900

    feature: add max_len, min_len config to datasets cls

--- dataset.py
+++ dataset.py
@@ -4,10 +4,24 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset, Subset
 
+from utils import chunkify
+
+
+def set_body(row, chunk):
+    row["body"] = chunk
+    return row
+
 
 class BlogDataset(Dataset):
-    def __init__(self, file_path):
-        self.data = pd.read_csv(file_path)
+    def __init__(self, file_path, max_len, min_len):
+        df = pd.read_csv(file_path)
+        df_dict = df.to_dict("records")
+        new_df = [
+            set_body(row.copy(), chunk)
+            for row in df_dict
+            for chunk in chunkify(row["body"], max_len, min_len)
+        ]
+        self.data = pd.DataFrame(new_df).set_index("title")
 
     def __getitem__(self, index):
         row = self.data.iloc[index]
@@ -17,13 +31,15 @@ class BlogDataset(Dataset):
         return len(self.data)
 
 
-def make_loader(mode, data_dir, batch_size, return_tags=False):
+def make_loader(mode, data_dir, batch_size, max_len, min_len, return_tags=False):
     assert mode in {
         "train",
         "val",
         "test",
     }, "`mode` must be one of 'train', 'val', or 'test'"
-    dataset = BlogDataset(os.path.join("data", data_dir, f"{mode}.csv"))
+    dataset = BlogDataset(
+        os.path.join("data", data_dir, f"{mode}.csv"), max_len, min_len
+    )
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     if not return_tags:
         return data_loader
